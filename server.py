976e2d64490712845e2b1b62f40e5d063fcc1ff3
Oğuzhan <ouzhan.ysl@gmail.com>
2021-01-16 15:09:44 +0300

    server and client

--- server.py
+++ server.py
@@ -4,14 +4,14 @@ s = socket.socket()
 
 port = 12345
 
-s.bind(('', port))      # bind() method: Specific ip and port so that it can listen to incoming requests on that ip and port.
+s.bind(("", port))      # bind() method: Specific ip and port so that it can listen to incoming requests on that ip and port.
 print(f"Socket binded to {port}")
 
-s.listen(5)
+s.listen(5)     # 5 connections are kept waiting if the server is busy and if a 6th socket trys to connect then the connection is refused.
 print("Socket is listening.")
 
 while True:
-    c, addr = s.accept()
+    c, addr = s.accept()    # Establish connection with client
     print(f"Got connection from {addr}")
 
     c.send("Thank you for connection")
