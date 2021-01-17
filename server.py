8a8811902f000f2f80ceea995458095a0960c418
Oğuzhan <ouzhan.ysl@gmail.com>
2021-01-17 12:07:36 +0300

    ops

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ print(f"Socket binded to {port}")
 s.listen(5)     # 5 connections are kept waiting if the server is busy and if a 6th socket trys to connect then the connection is refused.
 print("Socket is listening.")
 
+
 while True:
     c, addr = s.accept()    # Establish connection with client
     print(f"Got connection from {addr}")
