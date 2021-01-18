6d85e9c4d2adbc1210da27a946b0ab1447a42f38
Oğuzhan <ouzhan.ysl@gmail.com>
2021-01-18 21:28:25 +0300

    x

--- client.py
+++ client.py
@@ -1,10 +1,25 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket()
+s.connect((socket.gethostname(), 1234)) # First Parameter: localhost, Second Parameter: port
 
-port = 12345
+while True:
+    full_msg = ""
+    new_msg = True
+    while True:
+        msg = s.recv(16)  # receive data from the server
+        if new_msg:
+            print(f"New message length: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
 
-s.connect(('127.0.0.1', port))
+        full_msg += msg.decode("utf-8")
 
-print(s.recv(1024)) # receive data from the server
-s.close()   # close the connection
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full_msg recvd.")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+
+print(full_msg)
--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 import socket 
 
+HEADERSIZE = 10
+
 s = socket.socket()
-
-port = 12345
-
-s.bind(("", port))      # bind() method: Specific ip and port so that it can listen to incoming requests on that ip and port.
+port = 1234
+s.bind((socket.gethostname(), port))      # bind() method: Specific ip and port so that it can listen to incoming requests on that ip and port.
 print(f"Socket binded to {port}")
 
 s.listen(5)     # 5 connections are kept waiting if the server is busy and if a 6th socket trys to connect then the connection is refused.
 print("Socket is listening.")
 
-
 while True:
-    c, addr = s.accept()    # Establish connection with client
+    clientSocket, addr = s.accept()    # Establish connection with client
     print(f"Got connection from {addr}")
 
-    c.send("Thank you for connection")
-    c.close
+    msg = "Welcome to the server!"
+    print(f"{len(msg):<{HEADERSIZE}}"+msg)
+
+    clientSocket.send(bytes(msg, "utf-8"))
+    clientSocket.close()
