f6eb768846923f09e1db19cab6ec98055acc6f1d
Oğuzhan <ouzhan.ysl@gmail.com>
2021-01-22 13:21:59 +0300

    kaboom

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ HOST = socket.gethostbyname(socket.gethostname())
 PORT = 1234
 
 SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+SERVER.bind((HOST, PORT))
 
 def handler_client(conn, addr):
     print(f"[NEW CONNECTION]: {addr} connected.")
@@ -13,16 +13,25 @@ def handler_client(conn, addr):
     connected = True
     while connected:
         data = conn.recv(1024).decode('utf-8')
-        
+
+        if not data:
+            break
+
+        data = int(data)
+        msg = conn.recv(data).decode('utf-8')
+
+        print(f"[{addr}]:{msg}")
+        conn.send(data.encode('utf-8'))
+
+    conn.close()
+
+
 
 if __name__ == "__main__":
-    with SERVER as s:
-        print("[STARTING]: Server is starting...")
-        s.bind((HOST, PORT))
-        s.listen(5)  # 5 connections are kept waiting if the server is busy and if a 6th socket trys to connect then the connection is refused.
-        print(f"[LISTENING]: Server is listening on {HOST}")
-        
-        while True:
-            conn, addr = s.accept()
-            threading.Thread(target=handler_client, args=(conn, addr)).start()
-            print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}")
+    print("[STARTING]: Server is starting...")
+    SERVER.listen(5)    # 5 connections are kept waiting if the server is busy and if a 6th socket trys to connect then the connection is refused.
+    print(f"[LISTENING]: Server is listening on {HOST}")
+    while True:
+        conn, addr = SERVER.accept()
+        threading.Thread(target=handler_client, args=(conn, addr)).start()
+        print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}")
