ae8aaa9a296aaa8d91599e1cfcd273f55d6d6c23
Oğuzhan <ouzhan.ysl@gmail.com>
2021-01-31 13:31:36 +0300

    Update server.py
    Delete Nesil 1 and Nesil 2

--- server.py
+++ server.py
@@ -7,81 +7,9 @@ TCP_PORT = 9001
 BUFFER_SIZE = 1024
 
 TCP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-TCP_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # 
+TCP_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 TCP_SOCKET.bind((TCP_HOST, TCP_PORT))
-# threads = []    # 1. Nesil kullanıyor
 
-# --- 1. Nesil ---
-# class ClientThread(threading.Thread):
-#     def __init__(self, ADDR, SOCK):
-#         threading.Thread.__init__(self)
-#         self.IP = IP
-#         self.PORT = PORT
-#         self.SOCK = SOCK
-#         print(f"[NEW CONNECTION]: Got connection from {ADDR}")
-
-#     def run(self):
-#         filename='ServerAccessKey.json'
-#         f = open(filename, 'rb')
-#         while True:
-#             data = f.read(BUFFER_SIZE)
-#             while data:
-#                 self.SOCK.send(data)
-#                 #print('Sent ',repr(data))
-#                 data = f.read(BUFFER_SIZE)
-#             if not data:
-#                 f.close()
-#                 self.SOCK.close()
-#                 break
-
-# if __name__=="__main__":
-#     print("[STARTING]: Server is starting...")
-#     while True:
-#         TCP_SOCKET.listen(5)
-#         print("Waiting for incoming connections...")
-#         (CONN, ADDR) = TCP_SOCKET.accept() # ADDR == (IP,PORT)
-#         print('Got connection from ', ADDR)
-#         newthread = ClientThread(ADDR, CONN)
-#         newthread.start()
-#         threads.append(newthread)
-
-#     for t in threads:
-#         t.join()
-
-
-
-# --- 2. Nesil ---
-# def handler_client(conn, addr):
-#     print(f"[NEW CONNECTION]: Got connection from {addr}")
-#     filename='ServerAccessKey.json'
-#     f = open(filename, 'rb')
-#     while True:
-#         try:
-#             data = f.read(BUFFER_SIZE)
-#             while data:
-#                 conn.send(data)
-#                 # print(f"Sent {repr(data)}")
-#                 data = f.read(BUFFER_SIZE)
-#             if not data:
-#                 f.close()
-#                 conn.close()
-#                 break
-
-#         except Exception as err:
-#             print(err)
-
-# if __name__ == "__main__":
-#     print("[STARTING]: Server is starting...")
-#     TCP_SOCKET.listen(5)
-#     while True:
-#         print(f"\n[LISTENING]: Server is listening on {TCP_HOST}")
-#         conn, addr = TCP_SOCKET.accept()     # addr == (IP, PORT)
-#         threading.Thread(target=handler_client, args=(conn, addr)).start()
-#         print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}")
-
-
-
-# --- 3. Nesil ---
 class TCP_SERVER(threading.Thread):
     def __init__(self, conn, addr):
         threading.Thread.__init__(self)
@@ -91,7 +19,7 @@ class TCP_SERVER(threading.Thread):
 
     def checkFile(self):
         logging.debug(f"[{self.addr}]: make a checkFile request.")
-        filename='GoodBye.json'
+        filename='Server File/GoodBye.json'
         f = open(filename, 'rb')
         while True:
             data = f.read(BUFFER_SIZE)
@@ -109,7 +37,7 @@ class TCP_SERVER(threading.Thread):
 
     def doNothing(self):
         # logging.debug(f"[{self.addr}]: make a doNothing request.")
-        filename='GoodBye.json'
+        filename='Server File/GoodBye.json'
         f = open(filename, 'rb')
         len_data = f.read(BUFFER_SIZE)
         while len_data:
